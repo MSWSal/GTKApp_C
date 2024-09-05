a97b249e5f8aefd3faed2118ebccb57766d4a3fb
SahanSalgado <sahansalgado10@gmail.com>
2024-09-05 12:29:11 +0530

    Added 2 buttons in window

--- gtkapp.c
+++ gtkapp.c
@@ -17,4 +17,16 @@ int main(int argc, char * argv[]){
     gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
+    box=gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+
+    button = gtk_button_new_with_label("Click Me");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+    closeButton = gtk_button_new_with_label("Close");
+    g_signal_connect(closeButton, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_box_pack_start(GTK_BOX(box), button, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(box), closeButton, TRUE, TRUE, 0);
+
+    gtk_container_add(GTK_CONTAINER(window), box);
+
 }
